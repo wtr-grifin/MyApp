b60132ee69a0811c635bdaeae9208fb2c9805b50
wataru takada <w-takada@griffin-net.co.jp>
2020-06-12 11:00:47 +0900

    Update:

--- www/js/main.js
+++ www/js/main.js
@@ -21,75 +21,75 @@
       };
 
     }else if(page.matches('#level1-page')){
-      const wordsB = [
+      const wordsA = [
         'at',
         'an',
         'be',
-        'do',
-        'go',
-        'he',
-        'if',
-        'is',
-        'it',
-        'me',
-        'no',
-        'of',
-        'oh',
-        'so',
-        'to',
-        'we',
-        'add',
-        'aim',
-        'and',
-        'ask',
-        'bed',
-        'buy',
-        'can',
-        'cry',
-        'did',
-        'dog',
-        'eat',
-        'ear',
-        'egg',
-        'fly',
-        'few',
-        'fun',
-        'gym',
-        'has',
-        'how',
-        'kid',
-        'ink',
-        'not',
-        'off',
-        'put',
-        'see',
-        'six',
-        'sky',
-        'tab',
-        'tag',
-        'the',
-        'web',
-        'who',
-        'why',
-        'win',
-        'yes',
-        'yet',
-        'you',
-        'zoo',
+        // 'do',
+        // 'go',
+        // 'he',
+        // 'if',
+        // 'is',
+        // 'it',
+        // 'me',
+        // 'no',
+        // 'of',
+        // 'oh',
+        // 'so',
+        // 'to',
+        // 'we',
+        // 'add',
+        // 'aim',
+        // 'and',
+        // 'ask',
+        // 'bed',
+        // 'buy',
+        // 'can',
+        // 'cry',
+        // 'did',
+        // 'dog',
+        // 'eat',
+        // 'ear',
+        // 'egg',
+        // 'fly',
+        // 'few',
+        // 'fun',
+        // 'gym',
+        // 'has',
+        // 'how',
+        // 'kid',
+        // 'ink',
+        // 'not',
+        // 'off',
+        // 'put',
+        // 'see',
+        // 'six',
+        // 'sky',
+        // 'tab',
+        // 'tag',
+        // 'the',
+        // 'web',
+        // 'who',
+        // 'why',
+        // 'win',
+        // 'yes',
+        // 'yet',
+        // 'you',
+        // 'zoo',
       ];
-      let word = wordsB[Math.floor(Math.random() * wordsB.length)];
-      let loc;
-      let score;
-      let miss;
-      const timeLimit = 5 * 1000;
+      let word = wordsA[Math.floor(Math.random() * wordsA.length)];
+      let loc =0;
+      let score =0;
+      let miss =0;
+      const timeLimit = 10 * 1000;
       let startTime;
       let isPlaying = false;
+      // let readytime = 3;
     
       const target = document.getElementById('target');
       const scoreLabel = document.getElementById('score');
       const missLabel = document.getElementById('miss');
       const timerLabel = document.getElementById('timer');
-      
     
       function updateTarget() {
         let placeholder = '';
@@ -113,40 +113,65 @@
           timerLabel.textContent = 0.00;
           setTimeout(() =>{
             showResult();
+            miss = 0;
           }, 100);
-        
-          // target.textContent = 'click to replay';
-          if(timerLabel === 0.00){
-            document.querySelector('#navigator').pushPage("result-page");
-          }
+          target.textContent = 'もう一回だけやってみよう！';
         }
       }
+
+      
     
       function showResult() {
-        const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
-        //ページ遷移してから結果を表示させたい
-        // setTimeout(document.querySelector('#navigator').pushPage('level3-page'),5 * 1000);
+        const taipo = document.getElementById('taipo');
+        taipo.textContent = `${score} letters, ${miss} 回`;
+        // console.log(taipo);
+        document.getElementById('modal').classList.remove('hidden');
+        document.getElementById('mask').classList.remove('hidden');
+        
+        page.querySelector('#home-button').onclick = function() {
+          document.querySelector('#navigator').resetToPage('home.html');
+          score = 0;
+          miss = 0;
+        };
+        page.querySelector('#restart-button').onclick = function() {
+          document.querySelector('#navigator').resetToPage('level1.html');
+          score = 0;
+          miss = 0;
+        };
+        if(isPlaying !== true){
+          return ;
+        }
       }
-
-         window.addEventListener('click', () => {
+      
+      window.addEventListener('keydown', e =>{
+        if(e.keyCode === 13){
+          return isPlaying;
+        }
+      });
+      
+      //ゲーム中の正誤判定
+      window.addEventListener('keydown', e =>{
+        if(e.keyCode === 13){
+          loc = 0;
+          score = 0;
+          miss = 0;
+          return isPlaying;
+        }
         if(isPlaying === true){
           return;
         }
         isPlaying = true;
     
-        loc = 0;
-        score = 0;
-        miss = 0;
         scoreLabel.textContent = score;
         missLabel.textContent = miss;
-        word =wordsB[Math.floor(Math.random() * wordsB.length)];
+        word =wordsA[Math.floor(Math.random() * wordsA.length)];
     
         target.textContent = word;
         startTime = Date.now();
         updateTimer();
       });
-        
     
+      // ゲーム開始
       window.addEventListener('keydown', e => {
         if (isPlaying !== true) {
           return;
@@ -154,8 +179,8 @@
         if (e.key === word[loc]) {
           loc++;
           if(loc === word.length){
-           word = wordsB[Math.floor(Math.random() * wordsB.length)];
-            loc = 0;
+           word = wordsA[Math.floor(Math.random() * wordsA.length)];
+           loc = 0;
           }
           updateTarget();
           score++;
@@ -165,8 +190,7 @@
           missLabel.textContent = miss;
         }
       });
-    
-    } else if (page.matches('#level2-page')){
+    } else if (page.matches("#level2-page")){
       const wordsB = [
         'amen',
         'anti',
@@ -202,7 +226,7 @@
       let loc;
       let score;
       let miss;
-      const timeLimit = 5 * 1000;
+      const timeLimit = 2 * 1000;
       let startTime;
       let isPlaying = false;
     
@@ -235,18 +259,31 @@
           setTimeout(() =>{
             showResult();
           }, 100);
-        
-          target.textContent = 'click to replay';
+          target.textContent = 'もう一回だけやってみよう！';
         }
       }
+
+      
     
       function showResult() {
+        const taipo = document.getElementById('taipo');
         const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
-        //ページ遷移してから結果を表示させたい
-        alert(`${score} letters, ${miss} misses, ${accuracy.toFixed(2)}% accuracy!`);
-      }
-    
-      window.addEventListener('click', () => {
+        taipo.textContent =  `${miss} 回`;
+        document.getElementById('modal').classList.remove('hidden');
+        document.getElementById('mask').classList.remove('hidden');
+        
+          // ${score} letters, , ${accuracy.toFixed(2)}% accuracy!
+            page.querySelector('#home-button').onclick = function() {
+              document.querySelector('#navigator').resetToPage('home.html');
+            };
+            page.querySelector('#restart-button').onclick = function() {
+              document.querySelector('#navigator').resetToPage('level2.html');
+            };
+        }
+      window.addEventListener('keyup', e =>{
+        if(e.keyCode === 13 ){
+          return isPlaying;
+        }
         if(isPlaying === true){
           return;
         }
@@ -263,6 +300,13 @@
         startTime = Date.now();
         updateTimer();
       });
+
+      window.addEventListener('keydown', e =>{
+        if(e.keyCode === 13){
+          return isPlaying;
+        }
+      });
+        
     
       window.addEventListener('keydown', e => {
         if (isPlaying !== true) {
@@ -326,7 +370,7 @@
       let loc;
       let score;
       let miss;
-      const timeLimit = 5 * 1000;
+      const timeLimit = 30 * 1000;
       let startTime;
       let isPlaying = false;
     
@@ -359,20 +403,31 @@
           setTimeout(() =>{
             showResult();
           }, 100);
-        
-          target.textContent = 'click to replay';
+          target.textContent = 'もう一回だけやってみよう！';
         }
       }
+
+      
     
       function showResult() {
+        const taipo = document.getElementById('taipo');
         const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
-        //ページ遷移してから結果を表示させたい
-        alert(`${score} letters, ${miss} misses, ${accuracy.toFixed(2)}% accuracy!`);
-
-
-      }
-    
-      window.addEventListener('click', () => {
+        taipo.textContent =  `${miss} 回`;
+        document.getElementById('modal').classList.remove('hidden');
+        document.getElementById('mask').classList.remove('hidden');
+        
+          // ${score} letters, , ${accuracy.toFixed(2)}% accuracy!
+            page.querySelector('#home-button').onclick = function() {
+              document.querySelector('#navigator').resetToPage('home.html');
+            };
+            page.querySelector('#restart-button').onclick = function() {
+              document.querySelector('#navigator').resetToPage('level3.html');
+            };
+        }
+      window.addEventListener('keyup', e =>{
+        if(e.keyCode === 13 ){
+          return isPlaying;
+        }
         if(isPlaying === true){
           return;
         }
@@ -389,6 +444,13 @@
         startTime = Date.now();
         updateTimer();
       });
+
+      window.addEventListener('keydown', e =>{
+        if(e.keyCode === 13){
+          return isPlaying;
+        }
+      });
+        
     
       window.addEventListener('keydown', e => {
         if (isPlaying !== true) {
@@ -408,12 +470,13 @@
           missLabel.textContent = miss;
         }
       });
+
+      if (ons.platform.isIPhoneX()) {
+        document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
+        document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
+      }
     }
   });
-
-  if (ons.platform.isIPhoneX()) {
-    document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
-    document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
-  }
-
 }
+
+
